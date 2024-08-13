403abd6c7735f15d95d7d02e0108e40596b1b4bc
Mauri <mauri.monzon91@gmail.com>
2024-08-13 13:55:30 -0300

    StackNavigator

---
+++ src/presentation/navigation/StackNavigator.tsx
@@ -0,0 +1,22 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import {Pagina1Screen} from '../screens/Pagina1Screen';
+import {Pagina2Screen} from '../screens/Pagina2Screen';
+import {Pagina3Screen} from '../screens/Pagina3Screen';
+
+export type RootStackParams = {
+  Pagina1Screen: undefined;
+  Pagina2Screen: undefined;
+  Pagina3Screen: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Pagina1Screen" component={Pagina1Screen} />
+      <Stack.Screen name="Pagina2Screen" component={Pagina2Screen} />
+      <Stack.Screen name="Pagina3Screen" component={Pagina3Screen} />
+    </Stack.Navigator>
+  );
+};
