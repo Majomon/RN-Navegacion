0669b391f025a91c97db1fe2e0070cd1f2e215a5
Mauri <mauri.monzon91@gmail.com>
2024-08-13 14:59:07 -0300

    Drawer basico

--- src/NavegationApp.tsx
+++ src/NavegationApp.tsx
@@ -3,11 +3,13 @@ import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {StackNavigator} from './presentation/navigation/StackNavigator';
+import {MenuLateralBasico} from './presentation/navigation/MenuLateralBasico';
 
 export const NavegationApp = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <MenuLateralBasico />
+      {/*   <StackNavigator /> */}
     </NavigationContainer>
   );
 };
